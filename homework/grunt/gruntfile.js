c3e282ca0c1a7edaebded1f76f09a8cc0047404c
周吕华 <zhoulvhua@zhilun.com>
2020-06-04 21:01:01 +0800

    grunt

--- homework/grunt/gruntfile.js
+++ homework/grunt/gruntfile.js
@@ -3,8 +3,8 @@
  * @Author: liannian9
  * @Date: 2020-06-03 14:25:51
  * @LastEditors: liannian9
- * @LastEditTime: 2020-06-03 14:33:33
- */ 
+ * @LastEditTime: 2020-06-04 09:38:36
+ */
 // 实现这个项目的构建任务
 // 默认暴露一个函数
 // 因为在全局安装了grunt-cli
@@ -15,8 +15,7 @@ const loadGrunttasks = require('load-grunt-tasks')
 const sass = require('sass')
 
 const data = {
-  menus: [
-    {
+  menus: [{
       name: 'Home',
       icon: 'aperture',
       link: 'index.html'
@@ -32,8 +31,7 @@ const data = {
     {
       name: 'Contact',
       link: '#',
-      children: [
-        {
+      children: [{
           name: 'Twitter',
           link: 'https://twitter.com/w_zce'
         },
@@ -55,84 +53,176 @@ const data = {
   date: new Date()
 }
 module.exports = grunt => {
-   // 插件配置项通过grunt.initConfig来初始化
-   grunt.initConfig({
-        clean:{
-          temp:'temp/**',
-          dist:'dist/**'
-        },
-        sass:{
-          options:{
-              sourceMap:true,
-              implementation:sass
-          },
-          main:{
-              files:{
-                  'dist/assets/styles/main.css':'src/assets/styles/main.scss',
-              }
-          }
-       },
-       babel:{
-          options:{
-              presets:['@babel/preset-env']
-          },
-          main:{
-              files:{
-                  'dist/assets/scripts/main.js':'src/assets/scripts/main.js',
-              }
-          }
+  // 插件配置项通过grunt.initConfig来初始化
+  grunt.initConfig({
+    clean: {
+      temp: 'temp/**',
+      dist: 'dist/**',
+      temp2: '.tmp/**',
+    },
+    sass: {
+      options: {
+        sourceMap: true,
+        implementation: sass
       },
-      copy: {
-        main: {
-          files: [
-            //包括路径中的文件
-            {expand: true, src: ['public/**'], dest: 'dist'},
-      
-          ]
+      main: {
+        files: {
+          'dist/assets/styles/main.css': 'src/assets/styles/main.scss',
         }
+      }
+    },
+    babel: {
+      options: {
+        presets: ['@babel/preset-env']
       },
-      imagemin:{
-        dynamic: {
-          options: {
-            optimizationLevel: 1 //定义 PNG 图片优化水平
+      main: {
+        files: {
+          'dist/assets/scripts/main.js': 'src/assets/scripts/main.js',
+        }
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          //包括路径中的文件
+          {
+            expand: true,
+            src: ['public/**'],
+            dest: 'dist'
           },
-          files: [{
-              expand: true,
-              cwd: 'src/assets/images',//原图存放的文件夹
-              src: ['**'], // 优化 img 目录下所有 png/jpg/jpeg/gif图片
-              dest: 'dist/assets/images' // 优化后的图片保存位置，覆盖旧图片，并且不作提示
+
+        ]
+      }
+    },
+    imagemin: {
+      dynamic: {
+        options: {
+          optimizationLevel: 1 //定义 PNG 图片优化水平
+        },
+        files: [{
+            expand: true,
+            cwd: 'src/assets/images', //原图存放的文件夹
+            src: ['**'], // 优化 img 目录下所有 png/jpg/jpeg/gif图片
+            dest: 'dist/assets/images' // 优化后的图片保存位置，覆盖旧图片，并且不作提示
           },
           {
             expand: true,
-            cwd: 'src/assets/fonts',//原图存放的文件夹
-            src: ['**'], // 优化 img 目录下所有 png/jpg/jpeg/gif图片
-            dest: 'dist/assets/fonts' // 优化后的图片保存位置，覆盖旧图片，并且不作提示
-        },
-        ]
-        }
-      },
-      web_swig: {
-        options: {
-          swigOptions:{
-            cache: false
+            cwd: 'src/assets/fonts', //字体文件存放的文件夹
+            src: ['**'], // 优化 fonts 目录下所有 字体文件
+            dest: 'dist/assets/fonts' // 优化后的字体文件保存位置，覆盖旧图片，并且不作提示
           },
-          getData: function(tpl){
-              return data;
-          }
+        ]
+      }
+    },
+    web_swig: {
+      options: {
+        swigOptions: {
+          cache: false
         },
-        your_target:{
-
-          expand: true,
-          cwd: 'src',//原图存放的文件夹
-          src: ['*.html'], // 优化 img 目录下所有 png/jpg/jpeg/gif图片
-          dest: 'dist' // 优化后的图片保存位置，覆盖旧图片，并且不作提示
+        getData: function (tpl) {
+          return data;
         }
       },
+      your_target: {
 
-   })
-   
+        expand: true,
+        cwd: 'src', //原图存放的文件夹
+        src: ['*.html'], // 优化 img 目录下所有 png/jpg/jpeg/gif图片
+        dest: 'dist' // 优化后的图片保存位置，覆盖旧图片，并且不作提示
+      }
+    },
+    useminPrepare: {
+      html: 'dist/*.html',
+      // options: {
+      //   dest: 'dist',
+      // },
+      build: {
+        files: [{
+          src: 'dist/*.html'
+        }]
+      },
+      options: {
+        dest: 'dist',
+        flow: {
+            html: {
+                steps: {
+                    // TODO for libs.js block I don't want uglify!
+                    mycss: ['concat'],
+                    js: ['concat', 'uglifyjs'],
+                    css: ['concat', 'cssmin']
+                },
+                post: {}
+            }
+        }
+      }
+    },
+    usemin: {
+      html: ['dist/*.html'], // 注意此处是build/
+      options: {
+        assetsDirs: 'dist',
+        blockReplacements: {
+          // our 'replacement block'
+          mycss: function (block) {
+            console.log(block);
+            return '<script src="' + block.dest + '"></script>';
+          }
+          // no need to redefine default blocks
+        }
+      }
+    },
+    cssmin: {
+      options: {
+        compatibility: 'ie8', //设置兼容模式 
+        noAdvanced: true //取消高级特性 
+      },
+      compress: {
+        files: {
+          'dist/assets/styles/vendor.css': '.tmp/concat/assets/styles/vendor.css',
+          // 'dist/assets/styles/main.css':'.tmp/concat/assets/styles/main.css',
+        },
+
+      },
+      compress2: {
+        files: {
+          // 'dist/assets/styles/vendor.css':'.tmp/concat/assets/styles/vendor.css',
+          'dist/assets/styles/main.css': '.tmp/concat/assets/styles/main.css',
+        },
+
+
+      }
+    },
+    browserSync: {
+      bsFiles: {
+        src: 'dist/'
+      },
+      options: {
+        server: {
+          baseDir: "dist/"
+        },
+        watchTask: true,
+      }
+    },
+    watch: {
+      options: {
+        livereload: true,
+
+      },
+      src: {
+        files: ['src/**'],
+        tasks: ['compile'],
+        options: {
+          interrupt: true,
+        },
+      },
+    },
+  })
+
 
   //  grunt.loadNpmTasks('grunt-contrib-clean');
   loadGrunttasks(grunt) // 导入所有grunt插件
-   grunt.registerTask('default', ['clean', 'sass', 'babel', 'copy', 'imagemin', 'web_swig'])
+  grunt.registerTask('compile', ['sass', 'babel', 'copy', 'imagemin', 'web_swig', 'useminPrepare', 'concat', 'uglify', 'usemin'])
+  grunt.registerTask('dev', ['clean', 'sass', 'babel', 'copy', 'imagemin', 'web_swig', 'useminPrepare', 'concat', 'uglify', 'usemin','browserSync', 'watch'])
+
+  grunt.registerTask('build', ['clean', 'sass', 'babel', 'copy', 'imagemin', 'web_swig', 'useminPrepare', 'concat', 'uglify', 'cssmin', 'usemin'])
+
 }
